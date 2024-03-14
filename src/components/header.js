615afdc7869fef038ad3dfbe96321df987c86f9d
Henrique Bonatto <nattohen@gmail.com>
2024-03-14 09:26:40 -0300

    inicio front end e header

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+import './home/css/home.css';
+
+import logo from '../assets/img/logo.svg'
+import pesquisa from '../assets/img/pesquisa.svg'
+
+function Header(props) {
+  return (
+    <header className="headerHome">
+      <Link to="/"><img src={logo} /></Link>
+
+      <nav className="navBar">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <a className="navLink" href="">INICIO</a>
+        </Link>
+        <Link to="/armas" style={{ textDecoration: 'none' }}>
+          <a className="navLink" href=''>ARMAS</a>
+        </Link>
+        <Link to="/facas" style={{ textDecoration: 'none' }}>
+          <a className="navLink" href=''>FACAS</a>
+        </Link>
+        <Link to="/sobre" style={{ textDecoration: 'none' }}>
+          <a className="navLink" href=''>SOBRE</a>
+        </Link>
+
+        <div className="containerPesquisa">
+          <input className='inputPesquisa' type="text"/>
+          <img className='btnPesquisa' src={pesquisa} alt="pesquisar"/>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import './css/home.css';
+import Header from '../header';
+
+function Home(props) {
+  return (
+    <main className="containerHome">
+      <Header/>
+    </main>
+  )
+}
+
+export default Home
